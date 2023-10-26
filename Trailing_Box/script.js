64e925711a50834408c65032bf16eddbb9afcccf
Abhijeet Chauhan <aschauhan1052002@gmail.com>
2023-10-26 13:53:55 +0530

    improved a bit with comment explaining changes

--- Trailing_Box/script.js
+++ Trailing_Box/script.js
@@ -1,8 +1,14 @@
-var box = document.querySelector('#rect');
+var box = document.querySelector("#rect");
 
-window.addEventListener("mousemove",function(details){
-    gsap.to(box,{
-        left: details.clientX, //automatically in px I guess
-        // automatically adding ease parameter
-    })
-})
+window.addEventListener("mousemove", function (details) {
+  var mapvalue = gsap.utils.mapRange(
+    0,
+    window.innerWidth,
+    100 + box.getBoundingClientRect().width / 2,
+    window.innerWidth - (100 + box.getBoundingClientRect().width / 2),
+    details.clientX
+  ); //Here using the box width will ensure that the box will never go out of bounds and will atleast have 200px distance from the edge
+  gsap.to(box, {
+    left: mapvalue,
+  });
+});
